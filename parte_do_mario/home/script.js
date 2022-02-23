cb3c4710ccd380af36f7e3e210a75984cca8c5ed
Mário Roberto Lima Pinto <mrlimaroberto@gmail.com>
2022-02-23 12:07:07 -0300

    tentativa do carrousel

--- parte_do_mario/home/script.js
+++ parte_do_mario/home/script.js
@@ -18,4 +18,21 @@ $( ".hamburger" ).show();
 });
 
 });
+// -------------------------------
+//Lógica do carrousel
+let carousel = document.getElementsByClassName('carrousel');
+let skip = document.getElementsByClassName('skip');
+let slider = document.getElementsByClassName('sliders');
+let option = document.getElementsByClassName('options');
 
+let slidersIMGS = ["url(../../images/greg-bulla-CQKJRK7jwFE-unsplash.jpg)",
+                   "url(../../images/kellen-riggin-oA5Il37kCzc-unsplash.jpg)",
+                   "url(../../images/microsoft-365-hC_796Wu-VY-unsplash.jpg)"]
+
+for(let skiper = 0; skiper < slider.length; skiper++){
+    slider[skiper].style.backgroundImage = slidersIMGS[skiper];
+}
+
+function skipImage(){
+
+}
